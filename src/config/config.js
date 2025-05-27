331125c2cfdd62c90779d25294d8c33c6f89454d
yashchaud <yashchaudhari908@gmail.com>
2025-05-27 20:01:12 +0530

    asd

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
   appName: process.env.APP_NAME || "SNF",
   defaultUserRole: process.env.DEFAULT_USER_ROLE || "member",
   allowRegistration: process.env.ALLOW_REGISTRATION || true,
-  frontendUrl: process.env.FRONTEND_URL || "localhost:3000",
+  frontendUrl: process.env.FRONTEND_URL || "13.126.180.52",
 };
